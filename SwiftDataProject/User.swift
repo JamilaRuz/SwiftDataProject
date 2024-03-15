7bbfe41b12ec02ae1b854fdff8f11a6be0d3167c
Jamila <j.ruzimetova@gmail.com>
2024-03-15 18:45:36 -0400

    - SwiftData and CloudKit project

---
+++ SwiftDataProject/User.swift
@@ -0,0 +1,22 @@
+//
+//  Item.swift
+//  SwiftDataProject
+//
+//  Created by Jamila Ruzimetova on 3/15/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
